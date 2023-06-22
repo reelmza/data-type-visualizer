ab2071abbfb67d805a7f54b8a979a246b02be893
Jesse Moses <jessemoses71@gmail.com>
2023-06-22 23:43:39 +0100

    Added header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,16 @@
+import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <div className="relative w-[80%] md:w-[50%] lg:w-[20%] flex items-center justify-center mb-10">
-      <div className="text-3xl font-bold text-center">Data-Visualizer</div>
-      <div className="absolute h-5 w-9 flex items-center justify-center text-xs font-semibold text-white bg-red-600 py-[2px] rounded-md top-[-15px] right-10">
-        v1.0
+      <img
+        className="h-12  border-r border-gray-800 mr-3 pr-3"
+        src={logo}
+        alt="DTV logo"
+      />
+
+      <div className="flex flex-col justify-center text-gray-800">
+        <div className="font-bold text-lg leading-none">DTV</div>
+        <div className="">Data Type Visualizer</div>
       </div>
     </div>
   );
